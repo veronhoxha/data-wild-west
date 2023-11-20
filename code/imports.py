858452f497769e526eb7337d444ac69682e2036d
Veron <veronbhoxha@gmail.com>
2023-11-20 15:22:10 +0100

    cleaning code and update in imports file

--- code/imports.py
+++ code/imports.py
@@ -1,3 +1,5 @@
+####
+
 import pandas as pd
 import numpy as np
 import requests
@@ -7,7 +9,7 @@ import time
 from datetime import datetime
 from itertools import permutations
 
-####    Grammar Correction
+#### Grammar Correction
 
 import pkg_resources
 import symspellpy
@@ -28,3 +30,14 @@ import sys;
 sys.path.append("./libraries/")
 from fleiss_kappa import fleiss_kappa
 from label_studio_JSON_parser import parse_file
+
+#### 
+
+import re 
+from bs4 import BeautifulSoup 
+import regex as re
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from tqdm import tqdm
