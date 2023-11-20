0f759e5e4d71ac51fdea92ab8c5cc1f7867c62b6
const5craciun <59504800+const5craciun@users.noreply.github.com>
2023-11-20 13:33:36 +0100

    update for imports file

--- code/imports.py
+++ code/imports.py
@@ -6,3 +6,10 @@ import json
 import time
 from datetime import datetime
 from itertools import permutations
+import numpy as np
+import pkg_resources
+import symspellpy
+from symspellpy.symspellpy import SymSpell, Verbosity
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="pkg_resources")
