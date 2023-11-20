eae8010006f58a9a89de3ffe636a69a1476800f2
const5craciun <59504800+const5craciun@users.noreply.github.com>
2023-11-20 14:36:16 +0100

    update

--- code/imports.py
+++ code/imports.py
@@ -9,7 +9,8 @@ from itertools import permutations
 import numpy as np
 import pkg_resources
 import symspellpy
-from symspellpy.symspellpy import SymSpell, Verbosity
+from symspellpy import SymSpell, Verbosity
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="pkg_resources")
+
