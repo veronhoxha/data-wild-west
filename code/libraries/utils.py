4c01518a80479fd70fb527d9fc0e68d94710ac61
Veron <veronbhoxha@gmail.com>
2023-12-04 10:22:24 +0100

    combined all in one map

--- code/libraries/utils.py
+++ code/libraries/utils.py
@@ -30,9 +30,6 @@ nltk.download('averaged_perceptron_tagger', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
 
-# Calculating IAA
-import krippendorff
-
 # OTHERS
 from tqdm import tqdm
 from geopy.distance import geodesic
@@ -774,95 +771,6 @@ def fleiss_kappa(annotations, categories, labels):
 
     return k
 
-
-def krippendorff_alpha(annotations, categories):
-    '''
-    Author: Veron Hoxha
-    
-    TODO: ADD DESCRIPTION
-    
-    Calculating Krippendorff's Alpha for IAA (based on https://en.wikipedia.org/wiki/Krippendorff%27s_alpha) by using the python "krippendorff" library
-    '''
-    
-    # finding the number of annotators
-    max_ratings = max(annotations.groupby("ID").count().max())
-    # total number of ratings in one row (5 * 5 = 25) - 5 annotators and 5 categories
-    total_ratings_per_item = max_ratings * len(categories)
-
-    # filitering annotations to include those ID's which are repeated 5 times
-    filtered_annotations = annotations.groupby("ID").filter(lambda x: len(x) == 5)
-    overlapping_IDs = filtered_annotations["ID"].unique()
-
-    if len(overlapping_IDs) < 2:
-        raise Exception("We need at least 2 overlapping annotations to calculate IAA.")
-
-    data_matrix = []
-    
-    # look at each review ID
-    for id in overlapping_IDs:
-        group = annotations[annotations['ID'] == id]
-        row = []
-        
-        for category in categories:
-            # if any annotator did not rate in this category for this item, append nan
-            ratings = group[category].tolist() + [np.nan] * (max_ratings - group[category].count())
-            row.extend(ratings)
-
-        # ensure each row length equals the total number of possible ratings per item
-        row = row[:total_ratings_per_item]
-        data_matrix.append(row)
-
-    # matrix where each row is an item and each column is an annotator's rating
-    data_matrix = np.array(data_matrix, dtype=float)
-    alpha = krippendorff.alpha(reliability_data=data_matrix, level_of_measurement='ordinal')
-    
-    return alpha 
-
-
-### Calculating Krippendorff's Alpha seperatly for each category
-
-def krippendorff_alpha_per_category(annotations, categories):
-    '''
-    Author: Veron Hoxha
-    
-    TODO: ADD DESCRIPTION
-    
-    Calculating Krippendorff's Alpha seperatly for each category (based on https://en.wikipedia.org/wiki/Krippendorff%27s_alpha) by using the python "krippendorff" library
-    '''
-    
-    # finding the number of annotators
-    max_ratings = max(annotations.groupby("ID").count().max())
-    
-    # filitering the annotations which are not repeated 5 times
-    filtered_annotations = annotations.groupby("ID").filter(lambda x: len(x) == 5)
-    overlapping_IDs = filtered_annotations["ID"].unique()
-
-    if len(overlapping_IDs) < 2:
-        raise Exception("We need at least 2 overlapping annotations to calculate IAA.")
-
-    category_alphas = {}
-    
-    # iterating over each category to calculate Krippendorff's alpha separately
-    for category in categories:
-        data_matrix = []
-        
-        # look at each review ID
-        for id in overlapping_IDs:
-            group = annotations[annotations['ID'] == id]
-            ratings = group[category].tolist()
-
-            # appending NaNs to ensure that each item's ratings list has the same length
-            ratings += [np.nan] * (max_ratings - len(ratings))
-            data_matrix.append(ratings)
-
-        data_matrix = np.array(data_matrix, dtype=float)
-        # calculating Krippendorff's alpha for the current category and store it
-        alpha = krippendorff.alpha(reliability_data=data_matrix, level_of_measurement='ordinal')
-        category_alphas[category] = alpha
-
-    return category_alphas
-
-
 def parse_label_studio_file(filepath):
     '''
     TODO: ADD DESCRIPTION
@@ -1059,24 +967,47 @@ def lemmatize_with_postag(sentence):
     return " ".join(lemmatized_list)
 
 
-
-def get_rating_average(google_reviews):
+def get_icon(activity_type):
     '''
-    TODO: ADD DESCRIPTION
-    # calculating the average rating
-    '''
-    type_avg_ratings  = google_reviews.groupby(['lat','lng'])['rating'].mean().to_dict()
+    Author: Veron Hoxha
     
-    # choosing the color of the marker based on the average rating
-    def get_color_for_type(lat, lng):
-        avg_rating = type_avg_ratings.get((lat, lng), 0)
+    TODO: ADD DESCRIPTION
+    '''
+    
+    icons = {
+        "gym": "dumbbell", 
+        "fitness": "heartbeat", 
+        "outdoors": "tree",
+        "nature": "leaf", 
+        "running": "running", 
+        "ball_sports": "users",
+        "swimming": "swimmer", 
+        "yoga": "om", 
+        "dance": "music",
+        "martial_arts": "fist-raised"
+    }
+    
+    return icons.get(activity_type, "question-sign")
+
+def get_rating_average(data, grouping_field):
+    '''
+    Author: Veron Hoxha
+    
+    TODO: ADD DESCRIPTION
+    '''
+    # handle grouping by a tuple of columns
+    if isinstance(grouping_field, tuple):
+        avg_rating_dict = data.groupby(list(grouping_field))['rating'].mean().to_dict()
+    else:
+        avg_rating_dict = data.groupby(grouping_field)['rating'].mean().to_dict()
+
+    def get_color_for_location(key):
+        avg_rating = avg_rating_dict.get(key, 0)
         if avg_rating >= 4:
             return 'green'
         elif avg_rating >= 2:
             return 'orange'
         else:
             return 'red'
-        
-    
-    return get_color_for_type
 
+    return get_color_for_location
